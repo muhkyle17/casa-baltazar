c0fdd72b783dec6d78b0462b0b6d826579a97ed8
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-06-13 13:16:45 -0700

    Configure tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: '#1796C3',
+        teal: '#4ED8B2',
+        lightBlue: '#BAE7E7',
+        beige: '#EBE3AC',
+        yellow: '#FED57A',
+      },
+    },
   },
   plugins: [],
 }
